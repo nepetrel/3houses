091813c713276ee12f84ef54aed877a7d4805638
nepetrel <45583120+nepetrel@users.noreply.github.com>
2020-05-21 00:03:40 -0700

    Update randomTen.js

--- randomTen.js
+++ randomTen.js
@@ -12,7 +12,7 @@ var index = 0;
     index = Math.floor(Math.random()* titles.length);
     document.getElementById("title"+i).innerHTML=titles[index];
     document.getElementById("content"+i).innerHTML=contents[index];
-    document.getElementbyId("link"+i).innerHTML=links[index];
+    document.getElementById("link"+i).innerHTML=links[index];
  }
 }
 /*function uniq(a) {
